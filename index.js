9e5213a198dc0492489b64cd30b1a1e879d7a55f
Renan Dias <renansdiaz@gmail.com>
2015-09-15 08:19:23 -0300

    [JRD-328] Queue is being fetched; containers are being created

--- index.js
+++ index.js
@@ -1,4 +1,53 @@
 'use strict';
-var app = require('./app/app.js');
 
-app('Hi There!!!');
+var DockerContainer = require('carbono-docker').DockerContainer;
+var DockerManager   = require('carbono-docker').DockerManager;
+var sqsConsumer     = require('sqs-consumer');
+
+var queueUrl = 
+    'https://sqs.us-east-1.amazonaws.com/891008257771/homolog_carbono_paas';
+
+var DEFAULT_API_VERSION = '2012-11-05';
+var DEFAULT_REGION = 'us-east-1';
+
+var dockerManager = new DockerManager();
+
+var app = sqsConsumer.create({
+    queueUrl: queueUrl,
+    region: DEFAULT_REGION,
+    messageAttributeNames: ['imageName', 'appHash'],
+    handleMessage: function (message, done) {
+        var container = new DockerContainer({
+            image: message.MessageAttributes.imageName.StringValue,
+            hostConfig: {
+                "PortBindings": { 
+                    "4001/tcp": [{ "HostPort": "" }] }
+            }
+        });
+
+        var promise = dockerManager.createContainer(container);
+
+        promise
+            .then(function (containerId) {
+                var promise = dockerManager.getPortForContainerId(containerId);
+
+                promise
+                    .then(function (port) {
+                        console.log('PORT: ' + port);
+                    }, function (err) {
+                        console.log(err);
+                    });
+            }, function (err) {
+                console.log(err);
+            })
+            .done(function () {
+                done();
+            });
+    }
+});
+
+app.on('error', function (err) {
+    console.log(err);
+});
+
+app.start();
