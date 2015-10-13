d9826600167cceb6639a31f42d538a0627bce7cb
Renan Dias <renansdiaz@gmail.com>
2015-10-13 11:02:45 -0300

    [JRD-458] Added ops folder with ansible playbooks

--- index.js
+++ index.js
@@ -11,62 +11,73 @@ var DEFAULT_API_VERSION = '2012-11-05';
 var DEFAULT_REGION = 'us-east-1';
 var MARATHON_URL = process.env.MARATHON_URL;
 
-var marathon = new Marathon(MARATHON_URL);
+if (typeof MARATHON_URL === 'undefined') {
+    console.log("Environment variable MARATHON_URL cannot be empty!");
+    console.log("Define it before proceeding.");
+} else {
+    var marathon = new Marathon(MARATHON_URL);
 
-var app = sqsConsumer.create({
-    queueUrl: queueUrl,
-    region: DEFAULT_REGION,
-    messageAttributeNames: ['imageName', 'appHash'],
-    handleMessage: function (message, done) {
-        console.log("message received");
+    var app = sqsConsumer.create({
+        queueUrl: queueUrl,
+        region: DEFAULT_REGION,
+        messageAttributeNames: ['appHash', 'imageName', 'route'],
+        handleMessage: function (message, done) {
+            console.log("message received");
 
-        var app = {
-            id: message.MessageAttributes.imageName.StringValue,
-            cpus: 1,
-            mem: 64,
-            instances: 1,
-            container: {
-                type: "DOCKER",
-                docker: {
-                    "image": message.MessageAttributes.imageName.StringValue
-                },
-                volumes: [
-                    {
-                        containerPath: "/data",
-                        hostPath: "/data",
-                        mode: "RW"
-                    }
-                ]
-            },
-            args: [
-                "mongod", "--smallfiles"
-            ]
-        };
+            var app = {
+                id: message.MessageAttributes.imageName.StringValue,
+                cpus: 1,
+                mem: 64,
+                instances: 1,
+                container: {
+                    type: "DOCKER",
+                    docker: {
+                        "image": message.MessageAttributes.imageName.StringValue,
+                        "network": "BRIDGE", 
+                        "portMappings": [{
+                            "containerPort": 8080, 
+                            "hostPort": 31100, 
+                            "protocol": "tcp" 
+                        }]
+                    },
+                    volumes: [
+                        {
+                            containerPath: "/data",
+                            hostPath: "/code",
+                            mode: "RW"
+                        }
+                    ]
+                }
+            };
 
-        var promise = marathon.createApp(app);
+            var promise = marathon.createApp(app);
 
-        promise
-            .then(function (result) {
-                var id = result.id.split("/")[1];
-                var script = 'script/ansible.sh';
-                var args = [MARATHON_URL, id];
-                
-                execFile(script, args, function(err, stdout, stderr) {
+            promise
+                .then(function (result) {
+                    var marathonAppId = result.id.split("/")[1];
+                    var route = message.MessageAttributes.route.StringValue;
+                    var filename = route.replace(/\//g, "");
+                    var script = 'script/ansible.sh';
+
+                    var args = [MARATHON_URL, marathonAppId, route, filename];
+                    
+                    execFile(script, args, function(err, stdout, stderr) {
+                        console.log(err);
+                        console.log(stdout);
+                        console.log(stderr);
+                    });
+                }, function (err) {
                     console.log(err);
-                    console.log(stdout);
-                    console.log(stderr);
+                })
+                .done(function () {
+                    // done();
                 });
-            }, function (err) {
-                console.log(err);
-            })
-            .done(function () {
-                // done();
-            });
-    }
-});
+        }
+    });
 
-app.on('error', function (err) {
-    console.log(err);
-});
+    app.on('error', function (err) {
+        console.log(err);
+    });
 
-app.start();
+    app.start();
+}
